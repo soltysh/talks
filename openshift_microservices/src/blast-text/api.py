c4d4accc457dffc4e50041e64529d09757f0bead
Maciej Szulik <maszulik@redhat.com>
2015-09-22 23:47:53 +0200

    Enable CORS

--- openshift_microservices/src/blast-text/api.py
+++ openshift_microservices/src/blast-text/api.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 from flask_restful import Resource, Api
+from flask.ext.restful.utils import cors
 
 from db import Mongo
 
 app = Flask(__name__)
 api = Api(app)
+api.decorators = [cors.crossdomain(
+    origin="*", headers=['accept', 'Content-Type'],
+    methods=['HEAD', 'OPTIONS', 'GET', 'PUT', 'POST', 'DELETE'])]
 
 class BlastText(Resource):
 
