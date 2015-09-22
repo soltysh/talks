b69ad0ba1aebbcf38a956a5e2b804a5e9445d33f
Maciej Szulik <maszulik@redhat.com>
2015-09-22 23:49:29 +0200

    Initial steps in the Angular UI

--- openshift_microservices/src/blast-ui/js/app.js
+++ openshift_microservices/src/blast-ui/js/app.js
@@ -1,3 +1,20 @@
 (function() {
     var app = angular.module('blast-ui', [])
+
+    app.controller('BlastUIController', ['$http', function($http) {
+        this.search = '';
+        this.texts = {};
+        this.images = {};
+        this.videos = {};
+
+
+        var store = this;
+        this.search = function() {
+            url = 'http://localhost:8000/blast/api/v1.0/text/openshift';
+            $http.get(url).success(function(data){
+                store.texts = data.items;
+            });
+        }
+
+    }]);
 })();
