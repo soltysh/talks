cb81e8afeec6b0c6c29351bafe18c8f182db08e1
Maciej Szulik <maszulik@redhat.com>
2015-09-23 22:26:13 +0200

    Enabled all the services

--- openshift_microservices/src/blast-ui/js/app.js
+++ openshift_microservices/src/blast-ui/js/app.js
@@ -2,7 +2,7 @@
     var app = angular.module('blast-ui', [])
 
     app.controller('BlastUIController', ['$http', function($http) {
-        this.search = '';
+        this.searchText = '';
         this.texts = {};
         this.images = {};
         this.videos = {};
@@ -10,11 +10,24 @@
 
         var store = this;
         this.search = function() {
-            url = 'http://localhost:8000/blast/api/v1.0/text/openshift';
-            $http.get(url).success(function(data){
-                store.texts = data.items;
+            textURL = 'http://localhost:8000/blast/api/v1.0/text/' + encodeURIComponent(store.searchText);
+            $http.get(textURL).success(function(data){
+                store.texts = data;
             });
-        }
 
+            imagesURL = 'http://localhost:8000/blast/api/v1.0/image/' + encodeURIComponent(store.searchText);
+            $http.get(imagesURL).success(function(data){
+                store.images = data;
+            });
+
+            videosURL = 'http://localhost:8000/blast/api/v1.0/video/' + encodeURIComponent(store.searchText);
+            $http.get(videosURL).success(function(data){
+                store.videos = data;
+            });
+
+        }
+        this.noResults = function() {
+            return store.texts.length == 0 && store.images.length == 0 && store.videos.length == 0;
+        }
     }]);
 })();
