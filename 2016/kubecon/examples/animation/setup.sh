8312be405a5c1c451c1c929165e9b0a35ddcf277
Maciej Szulik <maszulik@redhat.com>
2016-03-09 16:59:55 +0100

    More demo tweaks

--- 2016/kubecon/examples/animation/setup.sh
+++ 2016/kubecon/examples/animation/setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash -e
 
+echo "Setting up data..."
 mkdir -p tmp
 python3 generate.py
 pushd tmp
 python3 -m http.server 1111 &>/dev/null &
+popd
 
+echo "Setting up rabbitmq..."
 kubectl create -f rabbitmq.yaml
+set +e
 while true; do
-    kubectl get pods|grep rabbitmq|grep Running
+    kubectl get pods | grep rabbitmq | grep Running
     if [ $? -eq 0 ]; then
         break
     fi
     sleep 1
 done
+set -e
 
 broker_ip=$(kubectl get svc/rabbitmq-service --template '{{.spec.clusterIP}}')
 broker_url=amqp://guest:guest@$broker_ip:5672
@@ -21,5 +26,6 @@ for i in {1..40}; do
     amqp-publish --url=${broker_url} -r renderer -p -b "http://192.168.121.118:1111/$(printf %02d.py $i)"
 done
 
-sed -i 's/BROKER_URL/${broker_url}/g' render.yaml
+echo "Setting up the job..."
+sed -i 's|BROKER_URL|'${broker_url}'|g' render.yaml
 kubectl create -f render.yaml
